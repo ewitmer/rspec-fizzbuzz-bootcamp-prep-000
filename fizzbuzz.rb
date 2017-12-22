c72a6540c74ca902ad1578014b583ba4e4b6a002
Erin Witmer <ecwitmer@gmail.com>
2017-12-22 02:30:35 +0000

    automatically backed up by learn

--- fizzbuzz.rb
+++ fizzbuzz.rb
@@ -3,7 +3,7 @@ def fizzbuzz(int)
     puts "FizzBuzz"
   end
   if int % 3 == 0 # if the number int is divisible by 3
-    puts "Fizz" # Go fizz
+    puts "Fizz" 
   end
   if int % 5 == 0 
     puts "Buzz"
