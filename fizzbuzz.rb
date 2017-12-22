9adcdab8841fb362c72c9cc2df113150c81bc172
Erin Witmer <ecwitmer@gmail.com>
2017-12-22 02:42:26 +0000

    automatically backed up by learn

--- fizzbuzz.rb
+++ fizzbuzz.rb
@@ -1,9 +1,9 @@
 def fizzbuzz(int)
   if int % 3 == 0 && int % 5 == 0
     return "FizzBuzz"
-  else if int % 3 == 0 # if the number int is divisible by 3
+  elsif int % 3 == 0 # if the number int is divisible by 3
     return "Fizz" 
-  else if int % 5 == 0 
+  elsif int % 5 == 0 
     return "Buzz"
   end
 end
